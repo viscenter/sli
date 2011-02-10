e2150d464916ea5fd5ba1fec7375051ca6680bc8
Matt Seabold <matthew.seabold@uky.edu>
2011-02-10 16:12:50 -0500

    Added script to all images in directory to same size

--- imageProcessing/README.txt
+++ imageProcessing/README.txt
@@ -1,10 +1,32 @@
 Image Processing:
 
 Binaries:
-	findPageSize <Page Image>
-		-Prints size of rectangle bounding the page to stdout
-	cropToPage <Page Image> <New Image> [<New Width> <New Height>]
-		-Finds the page within <Page Image>, either crops to the page or to the specified size, and saves out to <New Image>
+
+findPageSize <Page Image>
+	-Prints size of rectangle bounding the page to stdout
+
+cropToPage <Page Image> [Options]
+	Without options will crop image down to page
+	Default output file will be <Input File>_crop.png
+	-Options:
+		-o <file> -- supply name for output file
+		-h <height> -- suppy a specific height to crop image to
+		-w <width> -- supply a specific width to crop image to
+		-r -- Overwrite input image file with new cropped image
+				->Will be overridden by -o if supplied
+
+Wrap Scripts:
+
+cropToLargest <Directory>
+	Will take supplied directory containing page images, find the largest
+	page size, and crop all images to the same (largest) size.
+
+	Adds a set number of pixel border to the returned largest size. To change
+	the size of the border, change the BORDER variable at the beginning of the 
+	script
+
+	Files will be saved with the default names from cropToPage
+
 
 Dependecncies (Dependent upon the following packages):
 	libcv4
--- imageProcessing/src/cropToPage.cpp
+++ imageProcessing/src/cropToPage.cpp
@@ -1,26 +1,81 @@
 #include "pageFinder.h"
 #include <stdio.h>
 #include <iostream>
+#include <unistd.h>
+#include <string>
+#include <string.h>
 
 using namespace cv;
 using namespace std;
 
 int main(int argc, char** argv)
 {
-    if(argc < 3 || argc == 4 || argc > 5) {
-        cout << "Usage: cropToPage <file to crop> <new file name>[<new width> <new height>]" << endl;
-        return 0;
-    }
-    bool useSize = false;
+	//Initialize flags for options
+	int c;
+	char* outFile;
+	char* inFile;
     int inWidth,inHeight;
-    if(argc == 5) { //Size has been supplied
-        useSize = true;
-        inWidth = atoi(argv[3]);
-        inHeight = atoi(argv[4]);
-    }
+	bool f_overwrite = false;
+	bool f_height = false;
+	bool f_width = false;
+	bool f_outfile = false;
 
-    IplImage* src = cvLoadImage(argv[1]);
-    char* newFileName = argv[2];
+	while((c = getopt(argc,argv,"ro:w:h:")) != -1) {
+		switch(c) {
+			case 'r':
+				f_overwrite = true;
+				break;
+			case 'o':
+				f_outfile = true;
+				outFile = optarg;
+				break;
+			case 'w':
+				f_width = true;
+				inWidth = atoi(optarg);
+				break;
+			case 'h':
+				f_height = true;
+				inHeight = atoi(optarg);
+				break;
+		}
+				
+	}
+	if(optind == argc) {
+		cout << "Usage: cropToPage <Input File> [options] \n";
+		cout << "Options:\n";
+		cout << "	-o <File> -- Supply output file\n";
+		cout << "	-w <Width> -- Supply width for new image\n";
+		cout << "	-h <Height> -- Supply height for new image\n";
+		cout << "	-r -- Overwrite input image with new image\n";
+	}
+
+	else {
+		inFile = argv[optind];
+	}
+
+	if(f_outfile && f_overwrite)
+		printf("Output file supplied. Ignoring overwrite.\n");
+
+	
+	if(!f_outfile) {
+		if(f_overwrite)
+			outFile = inFile;
+		else { //Set to deafult output file [inFile]_crop.png
+			char str[1024];
+
+			size_t length = strlen(inFile)-4;
+
+			strncpy(str,inFile,length);
+			str[(int)length] = '\0';
+			strcat(str,"_crop.png");
+
+			outFile = str;
+		}
+	}
+	
+    
+
+    IplImage* src = cvLoadImage(inFile);
 
     CvPoint p1,p2;
     CvRect pageBound = findPageBound(src);
@@ -29,11 +84,10 @@ int main(int argc, char** argv)
     p2.x = pageBound.x + pageBound.width;
     p2.y = pageBound.y + pageBound.height;
 
-    if(useSize) {
-        if(pageBound.width > inWidth || pageBound.height > inHeight) {//Input size is smaller than size of page
-            printf("WARNING: input size (%dX%d) is less than size of page (%dX%d). Page is likely to be cut.\n",inWidth,inHeight,pageBound.width,pageBound.height);
+    if(f_width) {
+        if(pageBound.width > inWidth) {//Input width is smaller than size of page
+            printf("WARNING: input width (%d) is less than size of page (%d). Page is likely to be cut.\n",inWidth,pageBound.width);
         }
-        p1.y -= (inHeight - pageBound.height);
         p1.x -= (inWidth - pageBound.width) / 2;
         p2.x += (inWidth - pageBound.width) / 2;
         if((inWidth - pageBound.width) % 2 != 0) {//Difference is odd so move left over one extra pixel
@@ -42,12 +96,35 @@ int main(int argc, char** argv)
             else
                 p1.x -= 1;
         }
+        pageBound = cvRect(p1.x,p1.y,inWidth,pageBound.height);
+    }
 
-        pageBound = cvRect(p1.x,p1.y,inWidth,inHeight);
+    if(f_height) {
+        if(pageBound.height > inHeight) {//Input height is smaller than size of page
+            printf("WARNING: input height (%d) is less than size of page (%d). Page is likely to be cut.\n",inHeight,pageBound.height);
+        }
+
+		int difference = inHeight - pageBound.height;
+		int add=1;
+		if(difference < 0) {
+			difference = -difference;
+			add = -1;
+		}
+		int remaining = difference;
+
+		//Bottom bound is likely near bottom of image, so check to make sure we dont exceed the image height during expansion
+		while(p2.y < src->height-1 && remaining > difference/2) {
+			p2.y += add;
+			remaining--;
+		}
+		p1.y -= add*remaining;
+
+        pageBound = cvRect(p1.x,p1.y,pageBound.width,inHeight);
     }
     IplImage* cropped = cvCreateImage( cvSize(p2.x-p1.x, p2.y-p1.y), src->depth, 3);
     cvSetImageROI(src,pageBound);
     cvCopy(src,cropped);
-    cvSaveImage(newFileName,cropped);
+	printf("Saving image: %s\n",outFile);
+    cvSaveImage(outFile,cropped);
     return 0;
-}                                  
+}                                  //Set to deafult output file cropOut.png
